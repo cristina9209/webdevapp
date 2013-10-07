7f1773ed609feeb3efbf6dfa751e8fc6d736788f
cristina9209 <cristina9209@yahoo.com>
2013-10-07 22:11:09 +0300

    Create exemple.h

---
+++ exemple.h
@@ -0,0 +1,50 @@
+#include <iostream>
+
+// #1
+int identica(int x){
+	return x;
+}
+
+// #2
+int patrat(int x){
+	return x * x;
+}
+
+// #3
+int cub(int x) {
+	return x * x * x;
+}
+
+// #4
+int opus(int x) {
+	return (0 - x);
+}
+
+// #5
+int invers(int x) {
+	return (1 / x);
+}
+
+// #6
+int nul(int x) {
+	return 0;
+}
+
+// #7
+int plus1(int x) {
+	return (x+1);
+}
+
+// #8
+int plus2(int x) {
+	return (x+2);
+}
+// #9
+int plus3(int x){
+	return (x+3);
+}
+
+// #10
+int plus4(int x) {
+	return (x+4);
+}
